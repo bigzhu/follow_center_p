8feeb7fbdde5d2c0da9bb199276a3f02943e9f33
bigzhu <vermiliondun@gmail.com>
2016-12-04 23:28:18 +0800

    不再同步.git

--- cp.sh
+++ cp.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-rsync -rvz -e "ssh -p 22" ../lib_p_bz bigzhu@follow.center:/home/bigzhu/
-rsync -rvz -e "ssh -p 22" ./* bigzhu@follow.center:/home/bigzhu/follow_center_p/
+rsync -rvz --exclude '.git' -e "ssh -p 22" ../lib_p_bz bigzhu@follow.center:/home/bigzhu/
+rsync -rvz --exclude '.git' -e "ssh -p 22" ./* bigzhu@follow.center:/home/bigzhu/follow_center_p/
