8c2267ccddb6e06e4be8cb42735f8f93bbacad88
bigzhu <vermiliondun@gmail.com>
2017-05-21 17:31:40 +0800

    social name 不符合的不再查出来, 但是可能会有性能问题

--- create_view.sql
+++ create_view.sql
@@ -4,6 +4,7 @@ drop VIEW all_message;
  CREATE VIEW all_message AS
 
 
+ select * from (
  SELECT m.id,
     m.user_id,
     m.god_id,
@@ -23,14 +24,17 @@ drop VIEW all_message;
         when m.m_type = 'github' then g.github-> 'avatar'
         when m.m_type = 'instagram' then g.instagram-> 'avatar'
         when m.m_type = 'tumblr' then g.tumblr-> 'avatar'
-        when m.m_type = 'facebook' then g.tumblr-> 'facebook'
-    end as avatar
+        when m.m_type = 'facebook' then g.facebook-> 'avatar'
+    end as avatar,
+    case 
+        when m.m_type = 'twitter' then g.twitter-> 'name'
+        when m.m_type = 'github' then g.github-> 'name'
+        when m.m_type = 'instagram' then g.instagram-> 'name'
+        when m.m_type = 'tumblr' then g.tumblr-> 'name'
+        when m.m_type = 'facebook' then g.facebook-> 'name'
+    end as social_name
    FROM message m,
     god g
   WHERE m.god_name = g.name
-  
-  
- 
-
-
-
+  ) q
+  where social_name = to_jsonb(name)
